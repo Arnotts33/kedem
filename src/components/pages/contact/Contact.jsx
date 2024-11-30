3d80d4ecb52b85012cfa7457a57301a35d64085a
Arnotts33 <arnaudlht@gmail.com>
2024-11-30 18:58:39 +0100

    Minor CSS fixes

--- src/components/pages/contact/Contact.jsx
+++ src/components/pages/contact/Contact.jsx
@@ -1,7 +1,11 @@
 import styles from "./Contact.module.css";
 
 function Contact() {
-	return <div className={styles.container}>Contact</div>;
+	return (
+		<div className={styles.container}>
+			<h1>Contact</h1>
+		</div>
+	);
 }
 
 export default Contact;
