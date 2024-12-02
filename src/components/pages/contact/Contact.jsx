741cda39a5b8ab8be013aaa2118f178b4c6588f8
Arnotts33 <arnaudlht@gmail.com>
2024-12-02 11:52:49 +0100

    Contact - Done implementing contact form

--- src/components/pages/contact/Contact.jsx
+++ src/components/pages/contact/Contact.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import styles from "./Contact.module.css";
+import Button from "../../ui/buttons/Button";
 
 function Contact() {
 	useEffect(() => {
@@ -17,6 +18,66 @@ function Contact() {
 					sommes ravis de répondre à toutes vos demandes et de vous
 					accompagner.
 				</p>
+			</section>
+
+			<section className={styles.formSection}>
+				<form>
+					<div className={styles.grid}>
+						<div className={styles.field}>
+							<label htmlFor="lastName">Nom</label>
+							<input
+								type="text"
+								id="lastName"
+								name="lastName"
+								placeholder="Votre nom..."
+								required
+							/>
+						</div>
+						<div className={styles.field}>
+							<label htmlFor="firstName">Prénom</label>
+							<input
+								type="text"
+								id="firstName"
+								name="firstName"
+								placeholder="Votre prénom..."
+								required
+							/>
+						</div>
+						<div className={styles.field}>
+							<label htmlFor="email">Email</label>
+							<input
+								type="email"
+								id="email"
+								name="email"
+								placeholder="Votre email..."
+								required
+							/>
+						</div>
+						<div className={styles.field}>
+							<label htmlFor="phone">Téléphone</label>
+							<input
+								type="tel"
+								id="phone"
+								name="phone"
+								required
+							/>
+						</div>
+					</div>
+					<div className={styles.field}>
+						<label htmlFor="message">Message</label>
+						<textarea
+							id="message"
+							name="message"
+							cols={30}
+							rows={10}
+							placeholder="Votre message..."
+							required
+						></textarea>
+					</div>
+					<div className={styles.submitBtn}>
+						<Button type="submit">Envoyer</Button>
+					</div>
+				</form>
 			</section>
 		</div>
 	);
