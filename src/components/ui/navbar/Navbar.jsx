7c8fc4e19a07f8438eb08dab675b96ec7ea52729
Arnotts33 <arnaudlht@gmail.com>
2024-12-04 14:59:42 +0100

    Header - Added GSAP animation

--- src/components/ui/navbar/Navbar.jsx
+++ src/components/ui/navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import { NavLink } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
 import styles from "./Navbar.module.css";
 import navigation from "./index";
 import menuIcon from "../../../assets/icons/bars-solid.svg";
@@ -8,13 +10,26 @@ import closeMenuIcon from "../../../assets/icons/xmark-solid.svg";
 
 function Navbar() {
 	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+	const headerRef = useRef(null);
+
+	useGSAP(() => {
+		const tl = gsap.timeline();
+
+		tl.from(headerRef.current, {
+			opacity: 0,
+			y: -100,
+			duration: 2.2,
+			delay: 0.5,
+			ease: "power4.inOut",
+		});
+	}, []);
 
 	function closeMobileMenu() {
 		setIsMobileMenuOpen(false);
 	}
 
 	return (
-		<header className={styles.header}>
+		<header className={styles.header} ref={headerRef}>
 			<nav className={styles.navbar}>
 				<div className={styles.logo} onClick={closeMobileMenu}>
 					<NavLink to="/">
