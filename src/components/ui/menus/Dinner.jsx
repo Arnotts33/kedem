73dceaae06c6086755d0fdaf0e4bf996ed67ed7e
Arnaud Lahaut <arnaudlht@gmail.com>
2025-09-12 15:33:37 +0200

    update: menu prices

--- src/components/ui/menus/Dinner.jsx
+++ src/components/ui/menus/Dinner.jsx
@@ -5,21 +5,20 @@ function Dinner() {
 		<div className={styles.menuContainer}>
 			<h2 className={styles.menuTitle}>Menus du Soir</h2>
 			<p className={styles.menuDescription}>
-				Deux soirs par semaine, embarquez dans notre univers culinaire
-				inspiré de nos voyages et origines pour une découverte en
-				plusieurs étapes.
+				Deux soirs par semaine, embarquez dans notre univers culinaire inspiré
+				de nos voyages et origines pour une découverte en plusieurs étapes.
 			</p>
 			<div className={styles.menu}>
 				<h3>Menu en quatre étapes </h3>
-				<p>38 €</p>
+				<p>39 €</p>
 			</div>
 			<div className={styles.menu}>
 				<h3>Menu en cinq étapes</h3>
-				<p>43 €</p>
+				<p>45 €</p>
 			</div>
 			<div className={styles.menu}>
 				<h3>Menu en six étapes</h3>
-				<p>48 €</p>
+				<p>52 €</p>
 			</div>
 		</div>
 	);
